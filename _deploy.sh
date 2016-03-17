4f99dd1c7c6b6d2c0eb556df641a2d764b792838
nate-d-olson <nolson@nist.gov>
2016-03-17 13:51:44 -0400

    travis build

---
+++ _deploy.sh
@@ -0,0 +1,9 @@
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+cd book-output
+cp -r ../_book/* ./
+git add *
+git commit -m"Update the book"
+git push origin gh-pages
