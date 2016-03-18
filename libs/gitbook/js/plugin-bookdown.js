62dd67d30235f287d26b46b94d2c086f873fd63b
nate-d-olson <nolson@nist.gov>
2016-03-18 01:05:35 +0000

    Update the book

--- libs/gitbook/js/plugin-bookdown.js
+++ libs/gitbook/js/plugin-bookdown.js
@@ -175,20 +175,33 @@ require(["gitbook", "lodash", "jQuery"], function(gitbook, _, $) {
   });
 
   var bookBody = $('.book-body'), bookInner = bookBody.find('.body-inner');
-  $(document).on('servr:reload', function(e) {
-    // save scroll position before page is reloaded via servr
-    gs.set('bookInnerScrollTop', bookInner.scrollTop());
-    gs.set('bookBodyScrollTop', bookBody.scrollTop());
-  });
+  var saveScrollPos = function(e) {
+    // save scroll position before page is reloaded
+    gs.set('bodyScrollTop', {
+      body: bookBody.scrollTop(),
+      inner: bookInner.scrollTop(),
+      title: bookInner.find('.page-inner').find('h1,h2').first().text()
+    });
+  };
+  $(document).on('servr:reload', saveScrollPos);
+
+  // check if the page is loaded in the RStudio preview window
+  var inRStudio = function() {
+    var inIframe = true;
+    try { inIframe = window.self !== window.top; } catch (e) {}
+    if (!inIframe) return false;
+    return /^\/rmd_output\/[0-9]+\/$/.test(window.location.pathname);
+  };
+  if (inRStudio()) $(window).on('unload', saveScrollPos);
 
   $(document).ready(function(e) {
-    var pos = gs.get('bookBodyScrollTop');
-    if (pos) bookBody.scrollTop(pos);
-    pos = gs.get('bookInnerScrollTop');
-    if (pos) bookInner.scrollTop(pos);
+    var pos = gs.get('bodyScrollTop');
+    if (pos && pos.title === bookInner.find('.page-inner').find('h1,h2').first().text()) {
+      if (pos.body !== 0) bookBody.scrollTop(pos.body);
+      if (pos.inner !== 0) bookInner.scrollTop(pos.inner);
+    }
     // clear book body scroll position
-    gs.remove('bookBodyScrollTop');
-    gs.remove('bookInnerScrollTop');
+    gs.remove('bodyScrollTop');
   });
 
 });
